6da2cd3d648114df44eda142b88ef0acf759b23a
Rehan Khan <academicstuff9091@gmail.com>
2026-09-09 20:24:49 +0530

    Implement float division in float_division.c

--- Lab_01/float_division.c
+++ Lab_01/float_division.c
@@ -1,1 +1,16 @@
+#include <stdio.h>
+int main(){
 
+    int a;
+    int b;
+    float c;
+
+    printf("Enter 1st number: ");
+    scanf("%d",&a);
+    printf("Enter 2nd number: ");
+    scanf("%d", &b);
+    c=(float)a/b;
+    printf("Result: %f", c);
+    return 0;
+
+}
