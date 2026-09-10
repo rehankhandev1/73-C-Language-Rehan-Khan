b98dd329d202fd1f5b2163de6fc28636358c9abf
Rehan Khan <academicstuff9091@gmail.com>
2026-09-10 23:15:43 +0530

    Add multiplication program in Multiplication.c

---
+++ Lab_02/Multiplication.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int a, b, c;
+
+    printf("REHAN KHAN\n Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+
+    c = a * b;
+
+    printf("Multiplication = %d\n", c);
+
+    return 0;
+}
