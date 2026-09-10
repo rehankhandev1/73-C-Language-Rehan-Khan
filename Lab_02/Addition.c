535cc307b0492b2db606deeaa12f9d0ef54c3407
Rehan Khan <academicstuff9091@gmail.com>
2026-09-10 23:02:29 +0530

    Add Addition.c for summing two integers

---
+++ Lab_02/Addition.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main() {
+   int x, y, z;
+   printf("REHAN KHAN \nEnter two integers: ");
+   scanf("%d %d", &x, &y);
+    z=x+y;
+   printf("Sum: %d\n", z);
+   return 0;
+}
