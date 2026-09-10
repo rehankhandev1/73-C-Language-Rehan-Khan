56f94fd118c717e9f51966eafd731b94eaf10712
Rehan Khan <academicstuff9091@gmail.com>
2026-09-10 23:43:01 +0530

    Add files via upload

---
+++ Lab_02/Division.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main()
+{
+    int a;
+    int b;
+    int c;
+
+    printf("REHAN KHAN\nEnter 1st number: ");
+    scanf("%d", &a);
+    printf("Enter 2nd number : ");
+    scanf("%d", &b);
+
+    c=a/b;
+
+    printf("Division: %d", c);
+
+    return 0;
+
+
+
+
+}
