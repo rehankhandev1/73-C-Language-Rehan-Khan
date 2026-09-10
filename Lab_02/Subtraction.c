22ac86e9637a25b417f1986e7c7b4cfe2849d96e
Rehan Khan <academicstuff9091@gmail.com>
2026-09-10 23:55:08 +0530

    Add files via upload

---
+++ Lab_02/Subtraction.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    int a;
+    int b;
+    int c;
+
+    printf("Enter the 1st number: ");
+    scanf("%d", &a);
+    printf("Enter the 2nd number: ");
+    scanf("%d", &b);
+
+    c=a-b;
+
+    printf("Subtraction: %d", c);
+
+    return 0;
+
+}
