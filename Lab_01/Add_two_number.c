109b33d64c097c29498f64ca44e54ce5f07e3ee3
Rehan Khan <academicstuff9091@gmail.com>
2026-09-10 23:20:56 +0530

    Delete Lab_01/Add_two_number.c

--- Lab_01/Add_two_number.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main() {
-   int x, y, z;
-   printf("REHAN KHAN \nEnter two integers: ");
-   scanf("%d %d", &x, &y);
-    z=x+y;
-   printf("Sum: %d\n", z);
-   return 0;
-}
