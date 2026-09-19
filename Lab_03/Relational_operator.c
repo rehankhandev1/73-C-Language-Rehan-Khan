41c07966e3b0badd8b8b4da9435376360fd2d7d4
Rehan Khan <academicstuff9091@gmail.com>
2026-09-19 12:48:51 +0530

    Add files via upload

---
+++ Lab_03/Relational_operator.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int x = 16;
+    int y = 18;
+    printf("REHAN KHAN\n");
+    printf("x=%d,y=%d \n",x,y);
+    printf("is x equal to y?  :- %d\n    ",x==y);
+    printf("is x not equal to y? :- %d\n",x!=y);
+    printf("is x greater than y? :- %d\n",x>y);
+    printf("is x smaller than y? :- %d\n",x<y);
+    printf("is x greater than equals to y? :- %d \n",x>=y);
+    printf("is x smaller than equals to y? :- %d \n",x<=y);
+    
+    return 0;
+}
